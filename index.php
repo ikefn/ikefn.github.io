be6ab1ef00e939c32859a21c1f436e06ba9358a0
ike <90637140+ikefn@users.noreply.github.com>
2022-01-05 03:22:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 isaacfordjour
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us---iKranium Media Inc.</title>
-    <meta name="description" content="About ikranium media">
-    <meta name="robots" content="index, follow">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
 
 
@@ -16,26 +13,7 @@ isaacfordjour
       
 </head>
 <body>
-    <div class="topnavbar">
-  <div class="navbar">
-    <div class="link logo" style="padding: 10px;">
-      <a href=""><img src="logo.png" alt="" width="60px" height="60px"></a>
-    </div>
-    <div class="linkabout">
-        <a href="#about">About Us</a>
-        <a href="">Adertise with Us</a>
-    </div>
-  </div>
-</div> 
-<div class="main">
-    <div class="div1">
-    <h1>About the iKranium Media Inc.</h1>
-    <div class="imgpo">
-    <img src="images/div1.jpg" alt="ikranium.com--READ ALL NEWS"></div>
-    <div style="margin-top: 25px;">
-        <button class="visit" ><a href="">VISIT iKranium.com</a></button>
-    </div>
-</div>
+    Ishqud Fabergus
 
 </body>
 </html>
